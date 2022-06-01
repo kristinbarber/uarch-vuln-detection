4e1c67c2e9bd6ebce53eae9ddd859c3eb7388d68
Kristin Barber <kmbarber@owens-rw01.ten.osc.edu>
2022-06-01 10:24:54 -0400

    script mods

--- scripts/do_simulation.sh
+++ scripts/do_simulation.sh
@@ -5,29 +5,33 @@ ATYPE="$(echo $3 | cut -d'_' -f2)"
 
 #echo 'Compiling BOOM.....'
 #make CONFIG=SmallBoomConfig
-#mv simulator-chipyard-SmallBoomConfig simulator-chipyard-SmallBoomConfig-printall
 echo 'Starting simulation....'
 keyval=`cat scripts/keys/$1.key`
 pk=$RISCV/riscv64-unknown-elf/bin/pk
+
 if [ "$2" == "bearssl_comb" ]; then
 	if [ "$ATYPE" == "warmup" ]; then
-		bin_args="$pk $SIM_ROOT/uarch-leakage-detection/apps/bearssl-0.6/build/testcrypto_warmup $APP $keyval $4"
+		bin_args="$pk apps/bearssl-0.6/build/testcrypto_warmup $APP $keyval $4"
 	else
-		bin_args="$pk $SIM_ROOT/uarch-leakage-detection/apps/bearssl-0.6/build/testcrypto $APP $keyval $4"
+		bin_args="$pk apps/bearssl-0.6/build/testcrypto $APP $keyval $4"
 	fi
 elif [ "$2" == "bearssl_single" ]; then
-	bin_args="$pk $SIM_ROOT/uarch-leakage-detection/apps/bearssl-0.6/build/testcrypto-$3 $keyval"
+	bin_args="$pk apps/bearssl-0.6/build/testcrypto-$3 $keyval"
 elif [ "$2" == "bearssl_synthetic" ]; then
-	bin_args="$pk $SIM_ROOT/uarch-leakage-detection/apps/bearssl-0.6/microsampler_tests/$3 $keyval $4"
+	bin_args="$pk apps/bearssl-0.6/microsampler_tests/build/$3 $keyval $4"
 elif [ "$2" == "openssl" ]; then
 	bin_args="$pk $HOME/local/openssl/test/microsampler_test/exptest $3 $keyval"
 elif [ "$2" == "microbench" ]; then
-	bin_args="$SIM_ROOT/uarch-leakage-detection/apps/microbench/$3/$1/$3"
+	bin_args="apps/microbench/$3/$1/$3"
 fi
+
 echo "command: $SIM_ROOT/simulator-chipyard-SmallBoomConfig-$5 +verbose $bin_args > logs/$5/$2/$3/$4/$1/stdout.txt 2> logs/$5/$2/$3/$4/$1/out-all.log"
 time $SIM_ROOT/simulator-chipyard-SmallBoomConfig-$5 +verbose $bin_args > logs/$5/$2/$3/$4/$1/stdout.txt 2> logs/$5/$2/$3/$4/$1/out-all.log
+echo "Running spike-dasm on output log..."
 cat logs/$5/$2/$3/$4/$1/out-all.log | spike-dasm > logs/$5/$2/$3/$4/$1/out-all-asm.log
+echo "Compressing output log into gzip format..."
 gzip -f logs/$5/$2/$3/$4/$1/out-all-asm.log
+echo "Cleaning up....."
 rm logs/$5/$2/$3/$4/$1/out-all.log
 echo "done."
-echo `cat logs/$5/$2/$3/$4/$1/stdout.txt` | mail -s "simulation $5 $3 $1 complete" barber.m.kristin@gmail.com 
+#echo `cat logs/$5/$2/$3/$4/$1/stdout.txt` | mail -s "simulation $5 $3 $1 complete" barber.m.kristin@gmail.com 
--- scripts/do_stats.sh
+++ scripts/do_stats.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo 'Running state analysis....'
-time python scripts/stats.py logs/$8/$2/$3/$7/$1/uarch.pickle scripts/keys/$1.key logs/$8/$2/$3/$7/$1/sets.pickle logs/$8/$2/$3/$7/$1 $4 $5 $6 $7 > logs/$8/$2/$3/$7/$1/stats-$4_$5.log 2>&1; python3 scripts/miss_stats.py $3 $1 $8 $2; python3 scripts/simout_binkey.py $3 $1 $8 $2
+time python scripts/stats.py logs/$8/$2/$3/$7/$1/uarch.pickle scripts/keys/$1.key logs/$8/$2/$3/$7/$1/sets.pickle logs/$8/$2/$3/$7/$1 $4 $5 $6 $7 > logs/$8/$2/$3/$7/$1/stats-$4_$5.log 2>&1; python3 scripts/miss_stats.py $3 $1 $8 $2
+#; python3 scripts/simout_binkey.py $3 $1 $8 $2
